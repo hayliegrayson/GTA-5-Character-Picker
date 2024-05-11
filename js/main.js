f4929cfbcdda1f0b215aa0c7313dc48417809b49
Haylie Grayson <95108145+aaringrayson@users.noreply.github.com>
2024-05-11 01:01:28 -0400

    GTA 5 Character Picker

---
+++ js/main.js
@@ -0,0 +1,84 @@
+document.querySelector("#trevor").addEventListener("click", trevorBg)
+document.querySelector("#franklin").addEventListener("click", franklinBg)
+document.querySelector("#michael").addEventListener("click", michaelBg)
+document.querySelector("#lamar").addEventListener("click", lamarBg)
+document.querySelector("#chop").addEventListener("click", chopBg)
+document.querySelector("#wade").addEventListener("click", wadeBg)
+document.querySelector("#lester").addEventListener("click", lesterBg)
+document.querySelector("#amanda").addEventListener("click", amandaBg)
+document.querySelector("#jimmy").addEventListener("click", jimmyBg)
+document.querySelector("#tracey").addEventListener("click", traceyBg)
+document.querySelector("#tonya").addEventListener("click", tonyaBg)
+document.querySelector("#dave").addEventListener("click", daveBg)
+
+function trevorBg() {
+    document.body.style.backgroundImage = "url(imgs/trevor-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function franklinBg() {
+    document.body.style.backgroundImage = "url(imgs/franklin-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function michaelBg() {
+    document.body.style.backgroundImage = "url(imgs/michael-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+} 
+
+function lamarBg() {
+    document.body.style.backgroundImage = "url(imgs/lamar-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function chopBg() {
+    document.body.style.backgroundImage = "url(imgs/chop-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function wadeBg() {
+    document.body.style.backgroundImage = "url(imgs/wade-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function lesterBg() {
+    document.body.style.backgroundImage = "url(imgs/lester-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function amandaBg() {
+    document.body.style.backgroundImage = "url(imgs/amanda-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function jimmyBg() {
+    document.body.style.backgroundImage = "url(imgs/jimmy.png.jpg)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function traceyBg() {
+    document.body.style.backgroundImage = "url(imgs/tracey-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function tonyaBg() {
+    document.body.style.backgroundImage = "url(imgs/tonya-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
+
+function daveBg() {
+    document.body.style.backgroundImage = "url(imgs/dave-norton-bg.png)"
+    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundSize= "cover"
+}
